bd3b1fa4c41ebbd835ad5c6d35d2b5d988dfa3e2
Filippos Christianos <almak72@gmail.com>
2015-06-06 03:13:10 +0300

    New Start

--- db_interface.py
+++ db_interface.py
@@ -1,36 +1,33 @@
 import dropbox
 import os.path
 
-def DropboxConnect(config):
-	client = dropbox.client.DropboxClient(config["APP"]["token"])
-	return client
+def DropboxClient(token):
+    client = dropbox.client.DropboxClient(token)
+    return client
 
-def DropboxAuth(config):
-    if("token" in config["APP"].keys()):
-        access_token = config["APP"]["token"]
-    else:
-        access_token = None
+def DropboxAuth(appkey, appsecret, access_token=None):
     if(access_token == None):
-		    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(config["APP"]["key"], config["APP"]["secret"])
+        flow = dropbox.client.DropboxOAuth2FlowNoRedirect(appkey, appsecret)
+        # Have the user sign in and authorize this token
+        authorize_url = flow.start()
+        print ('1. Go to: ' + authorize_url)
+        print ('2. Click "Allow" (you might have to log in first)')
+        print ('3. Copy the authorization code.')
+        code = input("Enter the authorization code here: ").strip()
+        # This will fail if the user enters an invalid authorization code
+        access_token, user_id = flow.finish(code)
 
-		    # Have the user sign in and authorize this token
-		    authorize_url = flow.start()
-		    print ('1. Go to: ' + authorize_url)
-		    print ('2. Click "Allow" (you might have to log in first)')
-		    print ('3. Copy the authorization code.')
-		    code = input("Enter the authorization code here: ").strip()
-
-		    # This will fail if the user enters an invalid authorization code
-		    access_token, user_id = flow.finish(code)
     return access_token
 
+
+
 def DropboxUpload(client, rfile, path, revision=None):
-	f = open(rfile, "rb")
-	return client.put_file(path, f, revision)
+    f = open(rfile, "rb")
+    return client.put_file(path, f, revision)
 
 def DropboxDownload(client, wfile, cloudpath):
-	f, metadata = client.get_file_and_metadata(cloudpath)
-	out = open(wfile, 'wb')
-	out.write(f.read())
-	out.close()
-	return metadata
+    f, metadata = client.get_file_and_metadata(cloudpath)
+    out = open(wfile, 'wb')
+    out.write(f.read())
+    out.close()
+    return metadata
--- g2b.py
+++ g2b.py
@@ -1,120 +1,80 @@
 #!/usr/bin/env python3
 # Include the Dropbox SDK
 import dropbox
-import os.path
-import time
-from db_interface import DropboxConnect, DropboxUpload, DropboxDownload, DropboxAuth
-from tar_interface import make_tarfile, extract
-import tarfile
+import os
+import sys
 import argparse
-import shutil
-import sh
-import hashlib
-import json
 import git
-#from git import Repo
-# Get your app key and secret from the Dropbox developer website
+import shutil
+from db_interface import DropboxAuth, DropboxClient, DropboxDownload, DropboxUpload
+#def extract (file, location):
+from tar_interface import extract, make_tarfile
+ 
+appkey  = "zmuktk74z6ansu7"
+appsecret = "9c7vwi5kgmcbo6i"
 
-#connect to dropbox -- app validation etc
-
-
-currentRevision = None
-
-def pull(config):
-    print("PULL REQUEST")
-    tempdir = os.path.join(config["local"]["temp"], config['local']['basename'])
-    metadata = DropboxDownload(client, config["local"]["tarball"], os.path.join(config["cloud"]["path"],config["cloud"]["tarball"]),)
-    global currentRevision
-    currentRevision = metadata["rev"]
-    extract(config["local"]["tarball"], config["local"]["temp"])
-    #
-    # git = sh.git.bake(_cwd=config["local"]["path"])
-    # print(git.pull(os.path.join(config["local"]["temp"],"mycode"), "master"))
-
-    g = git.cmd.Git(config["local"]["path"])
-    g.pull("temp/mycode")
+LocalRepo = os.getcwd()
+tempDir = LocalRepo + "/temp"
+extractionDir = LocalRepo + "/extract"
+tempTar = LocalRepo + "/temp.tar.gz"
+CloudPath = "/project/mycode.tar.gz"
+client = None
 
 
-
-
-
-
-
-def push(config):
-    print("PUSH REQUEST")
-    pull(config)
-    make_tarfile(config["local"]["tarball"], config["local"]["path"])
-    metadata = DropboxUpload(client, config["local"]["tarball"], os.path.join(config["cloud"]["path"],config["cloud"]["tarball"]), revision=currentRevision)
-    print(metadata)
-    if(metadata['path'] == os.path.join(config["cloud"]["path"],config["cloud"]["tarball"])):
-        print("SUCCESS")
-    else:
-        print("RETRYING")
-        time.sleep(10)
-        push(config);
-
-
-def initialize():
-    local_dir=os.getcwd()
-    cloud_path=input('Please enter the dropbox path to use: ')
-    cloud_archive_name= os.path.split(os.getcwd())[1] + ".tar.gz"
-    configdata={
-            "APP":{
-                "key":"zmuktk74z6ansu7",
-                "secret":"9c7vwi5kgmcbo6i"
-            }
-    }
-    mytoken = DropboxAuth(configdata)
-    configdata={
-            "APP":{
-                "token": mytoken,
-                "key":"zmuktk74z6ansu7",
-                "secret":"9c7vwi5kgmcbo6i"
-            },
-            "local":{
-                "basename":os.path.split(os.getcwd())[1],
-                "path":local_dir,
-                "tarball": cloud_archive_name,
-                "temp":"temp"
-            },
-            "cloud":{
-                "path" : cloud_path,
-                "tarball" : cloud_archive_name,
-            }
-        }
-    with open('g2b.config', 'w') as jsonout:
-        json.dump(configdata, jsonout)
-    return configdata
-
-def configure():
+def clean():
+    shutil.rmtree(extractionDir)
+    shutil.rmtree(tempDir)
+def pull():
     try:
-        with open('g2b.config') as jsonin:
-            configdata=json.load(jsonin)
-    except Exception:
-        configdata=initialize()
-    finally:
-        return configdata
+        metadata = DropboxDownload(client, tempTar, CloudPath)
+    except dropbox.rest.ErrorResponse as e:
+        print("No repository in dropbox")
+        raise e
+    else:
+        print("Dropbox Repository Found. Pulling..")
+        if not os.path.exists(tempDir):
+            os.makedirs(tempDir)
+        if not os.path.exists(extractionDir):
+            os.makedirs(extractionDir)
+        extract(tempTar, extractionDir)
+        git.Git().pull(extractionDir + "/mycode")
+        
 
-def clean(config):
-    if(os.path.exists(config["local"]["temp"])):
-        shutil.rmtree(config["local"]["temp"])
-        os.remove(config["local"]["tarball"])
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--pull", action="store_true")
-parser.add_argument("--push", action="store_true")
-args = parser.parse_args()
-
-config = configure()
-clean(config)
-client = DropboxConnect(config)
-print(config)
-
-if(args.pull):
-    pull(config)
-if(args.push):
-    push(config)
-clean(config)
+        
+    
+def push():
+    os.chdir(tempDir)
+    extract(tempTar, extractionDir)
+    git.Git().clone(extractionDir+ "/mycode")
 
 
 
+
+def connect():
+    token = DropboxAuth(appkey, appsecret)
+    return DropboxClient(token)
+
+
+
+def main(arguments):
+
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--push', action='store_true')
+    group.add_argument('--pull', action='store_true')
+ 
+    args = parser.parse_args(arguments)
+    
+    global client
+    client = connect()
+
+
+    if(args.push == True):
+        push()
+    if(args.pull == True):
+        pull()
+    
+ 
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
